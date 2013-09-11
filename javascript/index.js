a7caae8ca901c02db4a1aede006b976182f11179
Sudhanshu Mishra <mrsud94@gmail.com>
2013-09-12 04:38:42 +0530

    Added breathing effect, removed unnecessary scripts

--- javascript/index.js
+++ javascript/index.js
@@ -1,13 +1,10 @@
 $(document).ready(function(){
-	console.log("Form Loaded");
     decrease();
     function decrease(){
         $('#release').animate({opacity:'0.6'},2000,increase);
-        console.log("decrease");
     }
 
     function increase(){
         $('#release').animate({opacity:'0.8'},2000, decrease);        
-        console.log("increase");
     }
 });		
