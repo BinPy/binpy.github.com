b0461667d818048113c9db169dd108ecc181bad7
Sudhanshu Mishra <mrsud94@gmail.com>
2013-09-17 22:36:47 +0530

    Updated site

--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     decrease();
     function decrease(){
-        $('#release').animate({opacity:'0.6'},2000,increase);
+        $('#release').animate({opacity:'0.70'},2000,increase);
     }
 
     function increase(){
-        $('#release').animate({opacity:'0.8'},2000, decrease);        
+        $('#release').animate({opacity:'0.95'},2000, decrease);        
     }
 });		
 
